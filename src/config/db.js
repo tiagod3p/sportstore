3ac5b83e321c197d34f0586159bf1918eb9ac410
BMS <saymon.titi@gmail.com>
2020-08-09 16:19:12 -0300

    FIX: Bug on images at Windows O.S.

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
     user: "tiago",
-    password:"goma123456@",
+    password:"123",
     host: "localhost",
     port: 5432,
     database: "sportstore"
